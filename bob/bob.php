c4b0b1a2d707d02815bcaa55fdfc96c1965da1ba
Joel Repta <Joel>
2019-03-10 01:39:36 -0500

    completed bob but not unicode tests

---
+++ bob/bob.php
@@ -0,0 +1,52 @@
+<?php
+
+class Bob {
+
+    private static $responses = [
+        "question" => "Sure.", 
+        "yell" => "Whoa, chill out!",
+        "yellQuestion" => "Calm down, I know what I'm doing!",
+        "empty" => "Fine. Be that way!",
+        "other" => "Whatever."
+    ];
+
+    function respondTo($input) {
+        $input = trim($input);
+        $response; 
+
+        if ($this->isEmpty($input)) {
+            $response = self::$responses['empty'];
+        }
+        elseif ($this->isQuestion($input)) {
+            if ($this->isYell($input)) {
+                $response = self::$responses['yellQuestion'];
+            }
+            else {
+                $response = self::$responses['question'];
+            }
+        }
+        elseif ($this->isYell($input)) {
+            $response = self::$responses['yell'];
+        }
+        else {
+            $response = self::$responses['other'];
+        }
+
+        return $response;
+    }
+
+    function isQuestion($input) {
+        return (substr($input, -1) == '?') ? true : false;
+    }
+
+    function isYell($input) {
+        $pattern = '/[^a-zA-Z]/';
+        return (ctype_upper(preg_replace($pattern, '', $input))) ? true : false;
+    }
+
+    function isEmpty($input) {
+        return ($input == '') ? true : false;
+    }
+}
+
+?>
